7143e438cd7935f9fd9c606a514faaadd0ac0b27
Beverly M <124820505+66006600@users.noreply.github.com>
2023-07-25 21:53:26 -0400

    Empezando

--- src/app.py
+++ src/app.py
@@ -30,11 +30,7 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
+    response_body = members   
 
     return jsonify(response_body), 200
 
